a02f31c3bb8da4a598b3a9c77ce3a7371f164b7b
Francisco Vargas <fav25@cl.cam.ac.uk>
2020-05-02 18:12:34 +0100

    initial implementations of inner loss

--- SC_IPFP/c_ipfp.py
+++ SC_IPFP/c_ipfp.py
@@ -1,15 +1,73 @@
-import jax.numpy 
-import autograd.numpy
+import jax.numpy  as np
+# import autograd.numpy
+import numpy as np
 from C_IPFP.sde_solvers import solve_sde_RK
 
 
 # np = jax.numpy
-np = autograd.numpy
+# np = autograd.numpy
 
 
 
 class cIPFP(object):
     
-    def __init__(self, X_0, X_1, batch_size=None,
-                 number_time_steps=16, sde_solver=solve_sde_RK):
-        pass
+    def __init__(self, X_0, X_1, batch_size=None, b_forward, b_backward
+                 number_time_steps=16, sde_solver=solve_sde_RK, sigma_sq=1):
+        
+        self.solver = sde_solver
+        
+        self.number_time_steps = number_time_steps
+        self.dt = 1.0 / number_time_steps
+        
+        self.batch_size = batch_size
+        
+        self.X_0 = X_0
+        self.X_1 = X_1
+        
+        self.b_forward = b_forward
+        self.b_backward = b_backward
+        
+        
+        self.sigma = lambda x,t: sigma_sq
+    
+    @staticmethod
+    def loss_for_trajectory(Xt, b_f, b_b, dt, forwards = True):
+        b_minus  = b_b(Xt)
+        b_plus = b_f(Xt)
+        
+        delta_Xt = Xt[:-1, :]  - Xt[1:, :]
+        
+        sign = 1.0 if forwards else -1.0
+        
+        ito_integrals = sign *  (b_plus[1:,:] - b_minus[:-1,:])  * delta_Xt
+        
+        time_integral = sign *  (b_plus**2 - b_minus**2) * dt
+        
+        return ito_integrals.sum() - 0.5 * time_integral.sum()
+        
+        
+    def sample_trajectory(self, X, forwards=True):
+        
+        # backwards discretisation has a sign flip         
+        b = self.b_forward if forward else (lambda x,t, theta: -self.b_backward(x,t,theta))
+        
+        return self.sde_solver(alfa=b, beta=self.sigma,
+                               dt=self.dt, X0=X,
+                               N=self.number_time_steps)
+    
+    def inner_loss(self, theta, forwards=True):
+        
+        X = self.X_0 if forward else self.X_1
+               
+        J = 0
+        
+        for x in X:
+            t, Xt = self.sample_trajectory(x, forwards=forwards)
+            
+            J += loss_for_trajectory(Xt, self.b_forward, self.b_backward, dt, forwards=forwards)
+        
+        J /= len(X)
+        
+        return J
+            
+        
--- SC_IPFP/sde_solvers.py
+++ SC_IPFP/sde_solvers.py
@@ -8,7 +8,7 @@ key = jax.random.PRNGKey(0)
 # key = None
 
 
-def solve_sde_RK(alfa=None, beta=None, X0=None, dt=1.0, N=100, t0=0.0, DW=None,
+def solve_sde_RK(alfa=None, beta=None, X0=None, dt=1.0, N=100, t0=0.0
                 key = key):
     """
             Kloeden - Numerical Solution of stochastic differential
@@ -33,28 +33,6 @@ def solve_sde_RK(alfa=None, beta=None, X0=None, dt=1.0, N=100, t0=0.0, DW=None,
                 (default: 100)
         t0    : The initial time of the solution
                 (default: 0)
-        DW    : The Wiener function in lambda notation
-                (default: gaussian np.random number generator, \
-                    [lambda Y, dt: randn(len(X0)) * np.sqrt(dt)] )
-    Examples:
-    ----------
-    == Simple Wiener Process:
-    dX = 0 + 1*dW
-    alfa = lambda X,t: 0
-    beta = lambda X,t: 1
-    t, Y = solve_sde(alfa=alfa, beta=beta, dt=1, N=1000)
-    == Stochastic Lorenz Equation:
-    dX = s (Y - X) + Y * dW1
-    dY = (r X - Y - X*Z) + dW2
-    dZ = (X*Y - b Z)  + dW3
-    xL = lambda X, t: 10.0 * (X[1] - X[0])  ;
-    yL = lambda X, t: 28.0 * X[0] - X[1] - X[0] * X[2] ;
-    zL = lambda X, t: X[0] * X[1] - 8.0/3.0 * X[2] ;
-    alfa = lambda X, t: np.array( [xL(X,t), yL(X,t), zL(X,t)] );
-    beta = lambda X, t: np.array( [     X[1],      1,      1] );
-    X0 = [3.4, -1.3, 28.3];
-    t, Y = solve_sde(alfa=alfa, beta=beta, X0=X0, dt=0.01, N=10000)
-    
     
     """
     
@@ -67,18 +45,25 @@ def solve_sde_RK(alfa=None, beta=None, X0=None, dt=1.0, N=100, t0=0.0, DW=None,
 #     import pdb; pdb.set_trace()
     X0 = randn(*alfa(0, 0).shape) if X0 is None else np.array(X0)
     print(X0)
-    DW = (lambda Y, dt: randn((len(X0))) * np.sqrt(dt)) if DW is None else DW
-    _, ti = np.zeros((N, len(X0))), np.arange(N)*dt + t0
-    Y = X0.reshape(1,-1)
-    _, Dn, Wn = X0, dt, 1
+#     DW = (lambda Y, dt: randn((len(X0))) * np.sqrt(dt)) if DW is None else DW
+    DWs  = randn(N-1, len(X0))  * np.sqrt(dt)
+    
+    
+    
+    Y, ti = np.zeros((N, len(X0))), np.arange(N)*dt + t0
+    Y = jax.ops.index_update(Y, jax.ops.index[0,:],  X0)
+    
+    Dn, Wn = dt, 1
 
     for n in range(N-1):
         t = ti[n]
-        a, b, DWn = alfa(Y[n, :], t), beta(Y[n, :], t), DW(Y[n, :], dt)
+        a, b, DWn = alfa(Y[n, :], t), beta(Y[n, :], t), DWs[n,:]
         # print Y[n,:]
-        newY = Y[n, :] + a*Dn + b*DWn*Wn + \
-                    0.5*(beta(Y[n, :] + b*np.sqrt(Dn), t) - b) * \
-                    (DWn**2.0 - Dn)/np.sqrt(Dn)
-#         print(Y.shape, newY.reshape(1,-1).shape)
-        Y = np.concatenate((Y, newY.reshape(1,-1)), axis=0)
+        newY = (  
+            Y[n, :] + a * Dn + b * DWn * Wn + 
+            0.5 * ( beta(Y[n, :] + b * np.sqrt(Dn), t) - b ) * 
+            (DWn**2.0 - Dn) / np.sqrt(Dn)
+        )
+
+        Y = jax.ops.index_update(Y, jax.ops.index[n+1,:],  newY)
     return ti, Y
