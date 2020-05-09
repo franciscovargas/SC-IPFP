9713de8056b00d890d108ab42d24e7cb8c4af39a
Francisco Vargas <fav25@cl.cam.ac.uk>
2020-05-09 17:18:07 +0100

    Added divergence version of path loss

---
+++ SC_IPFP/c_ipfp_div.py
@@ -0,0 +1,271 @@
+import sys
+sys.path.append("/auto/homes/fav25/jax/")
+
+import jax.numpy as np
+
+from SC_IPFP.sde_solvers import solve_sde_RK
+from SC_IPFP.utils import log_kde_pdf_per_point, silvermans_rule
+
+from jax.config import config
+from jax import jit, grad, random
+from jax.experimental import optimizers
+from jax.experimental import stax
+from jax.experimental.stax import Dense, Relu, LogSoftmax
+import numpy.random as npr
+import jax
+
+import itertools
+from functools import partial
+from tqdm.notebook import tqdm
+
+
+
+class cIPFP(object):
+    
+    def __init__(self, X_0, X_1, weights=[100], batch_size=None,  rng = jax.random.PRNGKey(0), 
+                nrng = npr.RandomState(0), number_time_steps=16, sde_solver=solve_sde_RK, sigma_sq=1, 
+                step_size = 0.001, num_epochs = 10, momentum_mass = 0.9, create_network=None):
+        
+        self.sde_solver = sde_solver
+        
+        self.number_time_steps = number_time_steps
+        self.dt = 1.0 / number_time_steps
+        
+        
+        self.batch_size_f = X_0.shape[0] if batch_size is None else batch_size
+        self.batch_size_b = X_1.shape[0] if batch_size is None else batch_size
+        
+        
+        self.X_0 = X_0
+        self.X_1 = X_1
+        
+        self.H_0 = silvermans_rule(X_0)
+        self.H_1 = silvermans_rule(X_1)
+        
+        _, self.dim = self.X_0.shape
+        
+        create_net = self.create_network if  create_network is None else create_network
+        
+        self.b_forward_init, self.b_forward = create_net(
+            self.dim, weights
+        )
+        self.b_backward_init, self.b_backward = create_net(
+            self.dim, weights
+        )
+        
+        self.sigma = (lambda X,t: sigma_sq)
+        
+        self.rng = rng
+        self.nrng = nrng
+        
+        self.opt_init_f, self.opt_update_f, self.get_params_f = (
+            optimizers.adam(step_size) #, mass=momentum_mass)
+        )
+        
+        self.opt_init_b, self.opt_update_b, self.get_params_b = (
+            optimizers.adam(step_size) #, mass=momentum_mass)
+        )
+        
+        num_complete_batches_f, leftover_f = divmod(self.X_0.shape[0], self.batch_size_f)
+        self.num_batches_f = num_complete_batches_f + bool(leftover_f)
+                                               
+        num_complete_batches_b, leftover_b = divmod(self.X_1.shape[0], self.batch_size_b)
+        self.num_batches_b = num_complete_batches_b + bool(leftover_b)
+        
+        self.theta_f = None
+        self.theta_b = None
+        
+        self.b_backward_ = jax.vmap(self.b_backward, in_axes=(None, 0))
+        self.b_forward_ = jax.vmap(self.b_forward, in_axes=(None, 0))
+        
+        self.b_backward_s =  lambda theta, x: np.squeeze(self.b_backward(x, theta))
+        self.b_forward_s = lambda theta, x: np.squeeze(self.b_forward(x, theta))
+        
+        self.div_backward_ = jax.vmap(partial(self.divergence, self.b_backward), in_axes=(None, 0))
+        self.div_forward_ = jax.vmap(partial(self.divergence, self.b_forward), in_axes=(None, 0))
+        
+    @staticmethod
+    def divergence(f, theta_, X_):
+
+        def my_div(f_):
+            jac = jax.jacrev(f_, 1)
+            return lambda t, x_: np.trace(jac(t, x_))
+
+        div  = ((jax.vmap(my_div(f), in_axes=(None, 0)) (theta_, X_ )))
+        return div
+    
+    @staticmethod
+    def create_network(dim, weights):
+        
+        model  = []
+        for weight in weights:
+            model.append(
+                Dense(weight)
+            )
+                        
+            model.append(
+                Relu
+            )
+            
+        
+        model.append(Dense(dim))
+    
+        init_random_params, predict = stax.serial(
+           *model
+        )
+        return init_random_params, predict
+    
+    def data_stream(self, forward=True):
+        rng = self.nrng
+        X = self.X_0 if forward else self.X_1
+        
+        batch_size = self.batch_size_f if forward else self.batch_size_b
+        num_batches = self.num_batches_f if forward else self.num_batches_b
+        
+        num_train = self.X_0.shape[0] if forward else self.X_1.shape[0]
+        while True:
+            perm = rng.permutation(num_train)
+            for i in range(num_batches):
+                batch_idx = perm[i * batch_size:(i + 1) * batch_size]
+                yield X[batch_idx] 
+        
+#     @partial(jit, static_argnums=(0,3))
+    def loss_for_trajectory(self, Xt, theta, W, forwards):
+        n_, *_ = Xt.shape
+        
+        if not forwards : Xt = Xt[: , ::-1, :]
+        
+        if forwards:
+            b_minus  = self.b_backward_(self.theta_b, Xt)
+            b_plus = self.b_forward_(theta, Xt)
+            div = self.div_backward_(self.theta_b, Xt)
+        else:
+            b_minus  = self.b_backward_(theta, Xt)
+            b_plus = self.b_forward_(self.theta_f, Xt)
+            div = -self.div_forward_(self.theta_f, Xt)
+        
+        sign = 1.0 if forwards else -1.0
+        
+        time_integral = (sign *  (b_plus - b_minus))**2 * self.dt 
+        out =  0.5 * time_integral.sum(axis=(1,2)) - div.sum(axis=(1))
+        
+        if np.isnan(out).any() or np.isinf(out).any():
+            import pdb; pdb.set_trace()
+        return out
+
+#     @partial(jit, static_argnums=(0,6,7,8,9,10,11))
+    def inner_loss_jit(self, theta, batch,  
+                       batch_terminal_empirical,
+                       H,  forwards):
+        
+        b = (
+            self.b_forward if forwards else (lambda X, theta: -self.b_backward(X, theta))
+        )
+        
+        t, Xt, *W = self.sde_solver(
+            alfa=b, beta=self.sigma, dt=self.dt, X0=batch,
+            N= self.number_time_steps, theta=theta
+        )
+        
+        cross_entropy = -log_kde_pdf_per_point(Xt[:,-1,:], batch_terminal_empirical, H)
+        main_term = self.loss_for_trajectory(Xt, theta, W, forwards)
+        
+        if np.isnan(cross_entropy).any() or np.isnan(main_term).any():
+            import pdb; pdb.set_trace()
+
+        J = np.mean(main_term + cross_entropy )
+        J = np.squeeze(J)
+        return J
+    
+    def inner_loss(self, theta, batch, forwards=True):
+                       
+        batch_terminal_empirical = next(self.data_stream(forward=not(forwards)))
+    
+        H = self.H_1 if forwards else self.H_0
+        return self.inner_loss_jit(
+            theta, batch,
+            batch_terminal_empirical, H ,  forwards
+        )
+
+#     @partial(jit, static_argnums=(0,1, 4))
+    def update(self, i, opt_state, batch, forwards=True):
+
+        get_params = self.get_params_f if forwards else self.get_params_b
+        params = get_params(opt_state)
+        
+        gradient = grad(self.inner_loss)(params, batch, forwards)
+    
+        opt_update  = self.opt_update_f if forwards else self.opt_update_b
+        return opt_update(i, gradient, opt_state)
+
+    def fit(self, IPFP_iterations=10, sub_iterations=10):     
+        
+        _, init_params_f = self.b_forward_init(self.rng, (-1, self.dim))                                             
+        opt_state_f = self.opt_init_f(init_params_f)
+        
+        _, init_params_b = self.b_backward_init(self.rng, (-1, self.dim))                                               
+        opt_state_b = self.opt_init_b(init_params_b)
+        
+        batches_f = self.data_stream(forward=True)
+        batches_b = self.data_stream(forward=False)
+        
+        loss_f, loss_b = [], []
+        
+        self.theta_f = self.get_params_f(opt_state_f)
+        self.theta_b = self.get_params_b(opt_state_b)
+        
+        for i in tqdm(range(IPFP_iterations)):
+
+            itercount = itertools.count()
+
+            for k in range(sub_iterations):
+                for _ in range(self.num_batches_b):
+                    batch_b =  next(batches_b)
+                    opt_state_b  = self.update(
+                        next(itercount), opt_state_b, batch_b, False
+                    )
+
+                params_b = self.get_params_b(opt_state_b)
+                lossb = self.inner_loss(params_b, batch_b, False)
+                print(f"loss b {lossb}")
+            loss_b.append(lossb)
+
+            self.theta_b = params_b
+
+            itercount = itertools.count()
+
+            for k in range(sub_iterations):
+                for _ in range(self.num_batches_f):
+                    batch_f =  next(batches_f)
+                    opt_state_f = self.update(
+                        next(itercount), opt_state_f, batch_f, True
+                    )
+                params_f = self.get_params_f(opt_state_f)
+                lossf = self.inner_loss(params_f, batch_f, True)
+                print(f"loss f {lossf}")
+            loss_f.append(lossf)
+
+
+            self.theta_f = params_f
+
+            self.loss_f = loss_f
+            self.loss_b = loss_b
+
+        
+        plt.plot(range(IPFP_iterations), loss_f, "g")
+        plt.show()
+        plt.plot(range(IPFP_iterations), loss_b, "b")
+        plt.show()
+    
+    #     @partial(jit, static_argnums=(0,2))
+    def transport_batch(self, batch_x, forwards):
+        theta = self.theta_f if forwards else self.theta_b    
+        b = self.b_forward if forwards else  lambda X, theta: -self.b_backward(X, theta)
+
+        t, Xt = self.sde_solver(
+            alfa=b, beta=self.sigma, dt=self.dt, 
+            X0=batch_x.reshape(-1,self.dim), N=self.number_time_steps, theta=theta
+        )
+
+        return Xt[:,-1,:]
+            
