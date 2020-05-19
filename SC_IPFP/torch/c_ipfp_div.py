6270d15503b03920c0c99a96e3a094a3c94ce059
Francisco Vargas <fav25@cl.cam.ac.uk>
2020-05-19 18:38:07 +0100

    Debugging torch implementation

--- SC_IPFP/torch/c_ipfp_div.py
+++ SC_IPFP/torch/c_ipfp_div.py
@@ -1,31 +1,20 @@
-import sys
-sys.path.append("/auto/homes/fav25/jax/")
+import torch
+import math
+from SC_IPFP.torch.utils import NN, log_kde_pdf_per_point, silvermans_rule
+from SC_IPFP.torch.sde_solvers_time import solve_sde_RK
 
-import jax.numpy as np
-
-from SC_IPFP.sde_solvers_time import solve_sde_RK
-from SC_IPFP.utils import log_kde_pdf_per_point, silvermans_rule
-
-from jax.config import config
-from jax import jit, grad, random
-from jax.experimental import optimizers
-from jax.experimental import stax
-from jax.experimental.stax import Dense, Relu, LogSoftmax
-import numpy.random as npr
-import jax
+from tqdm.notebook import tqdm
 
 import itertools
 from functools import partial
-from tqdm.notebook import tqdm
-
 
 
 class cIPFP(object):
     
-    def __init__(self, X_0, X_1, weights=[100], batch_size=None,  rng = jax.random.PRNGKey(100), rng_b = jax.random.PRNGKey(10), 
-                nrng = npr.RandomState(0), number_time_steps=16, sde_solver=solve_sde_RK, sigma_sq=1, 
+    def __init__(self, X_0, X_1, weights=[100], batch_size=None,  rng = None, rng_b = None, 
+                nrng = None, number_time_steps=16, sde_solver=solve_sde_RK, sigma_sq=1, 
                 step_size = 0.001, num_epochs = 10, momentum_mass = 0.9, create_network=None,
-                log_prob=log_kde_pdf_per_point):
+                log_prob=log_kde_pdf_per_point, optimizer=torch.optim.SGD):
         
         self.sde_solver = sde_solver
         
@@ -35,98 +24,48 @@ class cIPFP(object):
         
         self.batch_size_f = X_0.shape[0] if batch_size is None else batch_size
         self.batch_size_b = X_1.shape[0] if batch_size is None else batch_size
+        with torch.cuda.device(0):
         
-        
-        self.X_0 = X_0
-        self.X_1 = X_1
-        
-        self.H_0 = silvermans_rule(X_0)
-        self.H_1 = silvermans_rule(X_1)
-        
-        _, self.dim = self.X_0.shape
-        
-        create_net = self.create_network if  create_network is None else create_network
-        
-        self.b_forward_init, self.b_forward = create_net(
-            self.dim, weights
-        )
-        self.b_backward_init, self.b_backward = create_net(
-            self.dim, weights
-        )
-        
-        self.sigma = (lambda X,t: sigma_sq)
-        
-        self.rng = rng
-        self.rng_b = rng_b
-        self.nrng = nrng
-        
-        self.opt_init_f, self.opt_update_f, self.get_params_f = (
-            optimizers.adam(step_size) #, mass=momentum_mass)
-        )
-        
-        self.opt_init_b, self.opt_update_b, self.get_params_b = (
-            optimizers.adam(step_size) #, mass=momentum_mass)
-        )
-        
-        num_complete_batches_f, leftover_f = divmod(self.X_0.shape[0], self.batch_size_f)
-        self.num_batches_f = num_complete_batches_f + bool(leftover_f)
-                                               
-        num_complete_batches_b, leftover_b = divmod(self.X_1.shape[0], self.batch_size_b)
-        self.num_batches_b = num_complete_batches_b + bool(leftover_b)
-        
-        self.theta_f = None
-        self.theta_b = None
-        
-        self.b_backward_ = jax.vmap(self.b_backward, in_axes=(None, 0))
-        self.b_forward_ = jax.vmap(self.b_forward, in_axes=(None, 0))
-        
-        self.b_backward_s =  lambda theta, x: np.squeeze(self.b_backward(x, theta))
-        self.b_forward_s = lambda theta, x: np.squeeze(self.b_forward(x, theta))
-        
-        self.div_backward_ = jax.vmap(partial(self.divergence, self.b_backward), in_axes=(None, 0))
-        self.div_forward_ = jax.vmap(partial(self.divergence, self.b_forward), in_axes=(None, 0))
-        
-        self.first_iter_fac = 0.0
-        
-        self.log_prob = log_prob
-        
-        self.burn_in = False
-        
-    @staticmethod
-    def divergence(f, theta_, X_):
+            self.X_0 = X_0.requires_grad_()
+            self.X_1 = X_1.requires_grad_()
 
-        def my_div(f_):
-            jac = jax.jacfwd(f_, 1)
-            # this implementation assumes the last cordinate is time
-            # we dont want that term in the div
-            return lambda t, x_: np.trace(jac(t, x_)[:-1,:-1])
+            self.H_0 = silvermans_rule(X_0)
+            self.H_1 = silvermans_rule(X_1)
 
-        div  = ((jax.vmap(my_div(f), in_axes=(None, 0)) (theta_, X_ )))
-        return div
-    
-    @staticmethod
-    def create_network(dim, weights):
-        
-        model  = []
-        for weight in weights:
-            model.append(
-                Dense(weight)
+            _, self.dim = self.X_0.shape
+
+            weights = weights + [self.dim]
+
+
+            self.b_forward = NN(self.dim + 1, weights).double().cuda()
+            self.b_backward = NN(self.dim + 1, weights).double().cuda()
+
+            self.theta_f = list(self.b_forward.parameters())
+            self.theta_b = list(self.b_backward.parameters())
+
+            self.sigma = sigma_sq
+
+            self.optimizer_f = optimizer(
+                self.theta_f, lr=step_size
             )
-                        
-            model.append(
-                Relu
+
+            self.optimizer_b = optimizer(
+                self.theta_b, lr=step_size
             )
-            
-        
-        model.append(Dense(dim))
-    
-        init_random_params, predict = stax.serial(
-           *model
-        )
-        return init_random_params, predict
+
+            num_complete_batches_f, leftover_f = divmod(self.X_0.shape[0], self.batch_size_f)
+            self.num_batches_f = num_complete_batches_f + bool(leftover_f)
+
+            num_complete_batches_b, leftover_b = divmod(self.X_1.shape[0], self.batch_size_b)
+            self.num_batches_b = num_complete_batches_b + bool(leftover_b)
+
+            self.first_iter_fac = 0.0
+
+            self.log_prob = log_prob
+
+            self.burn_in = False
     
     def data_stream(self, forward=True):
-        rng = self.nrng
         X = self.X_0 if forward else self.X_1
         
         batch_size = self.batch_size_f if forward else self.batch_size_b
@@ -134,66 +73,71 @@ class cIPFP(object):
         
         num_train = self.X_0.shape[0] if forward else self.X_1.shape[0]
         while True:
-            perm = rng.permutation(num_train)
+            perm = torch.randperm(X.size()[0])
             for i in range(num_batches):
                 batch_idx = perm[i * batch_size:(i + 1) * batch_size]
                 yield X[batch_idx] 
         
-#     @partial(jit, static_argnums=(0,4))
-    def loss_for_trajectory(self, Xt, theta, W, forwards):
+    def loss_for_trajectory(self, Xt, forwards):
         n_, *_ = Xt.shape
         
-        if not forwards : Xt = Xt[: , ::-1, :]
+        if not forwards :
+            Xt = Xt.flip(1)
         
         if forwards:
-            b_minus  = self.b_backward_(self.theta_b, Xt) 
-            b_plus = self.b_forward_(theta, Xt)
-            div = self.div_backward_(self.theta_b, Xt) 
+            b_minus  = self.b_backward.forward( Xt)
+            b_plus = self.b_forward.forward(Xt)
+            div = divergence(b_minus, Xt)
         else:
-            b_minus  = self.b_backward_(theta, Xt)
-            b_plus = self.b_forward_(self.theta_f, Xt) * self.first_iter_fac 
-            div = -self.div_forward_(self.theta_f, Xt) * self.first_iter_fac 
-        
+            b_minus  = self.b_backward.forward(Xt)
+            b_plus = self.b_forward.forward(Xt) * self.first_iter_fac 
+            div = -divergence(b_plus, Xt) * self.first_iter_fac 
+
+            
         sign = 1.0 if forwards else -1.0
         
         time_integral = (sign *  (b_plus - b_minus))**2 * self.dt 
-        out =  0.5 * time_integral.sum(axis=(1,2)) - (div.sum(axis=(1)) *  self.dt )
-
+        out =  0.5 * time_integral.sum(axis=(1,2))  - (div.sum(axis=(1)) *  self.dt )
+        
         return out
 
-#     @partial(jit, static_argnums=(0,5))
-    def inner_loss_jit(self, theta, batch,  
+
+    def inner_loss_jit(self, batch,  
                        batch_terminal_empirical,
-                       H,  forwards):
+                       forwards):
         
         b = (
-            self.b_forward if forwards else (lambda X, theta: -self.b_backward(X, theta))
+            self.b_forward.forward
+            if forwards else (lambda X: -self.b_backward.forward(X))
         )
-        
-#         path_mode = lambda x:x if not(self.burn_in) else lambda x: not(x)
         
         t, Xt, *W = self.sde_solver(
             b, self.sigma, batch, self.dt, 
             self.number_time_steps, 0,
-            self.rng, theta, False, forwards
+            None, False, forwards
         )
         Ht = silvermans_rule(Xt[:,-1,:-1])
         H = silvermans_rule(batch_terminal_empirical)
         
-        main_term = self.loss_for_trajectory(Xt, theta, W, forwards)
-#         entropy = 0 #-log_kde_pdf_per_point(Xt[:,-1,:-1], Xt[:,-1,:-1], Ht) *  0
+        main_term = self.loss_for_trajectory(Xt, forwards)
+
         if not self.burn_in:
+            xx = torch.linspace(-30, 30 , 1000).reshape(-1,1).double().cuda()
+            yy = self.log_prob( xx, batch_terminal_empirical, H)  
+            plt.plot(
+                xx.detach().cpu().numpy(),
+                np.exp(yy.detach().cpu().numpy())
+            )
+            plt.show()
             cross_entropy = -self.log_prob( Xt[:,-1,:-1], batch_terminal_empirical, H)             
         else:
             cross_entropy = -self.log_prob(batch_terminal_empirical, Xt[:,-1,:-1], Ht)
-#             main_term = 0 
 
-        J = np.mean(main_term + 2.0 * cross_entropy )
-        J = np.squeeze(J)
+        J = torch.mean(main_term +  cross_entropy )
+        J = torch.squeeze(J)
         return J
     
-#     @partial(jit, static_argnums=(0, 3))
-    def inner_loss(self, theta, batch, forwards=True):
+    def inner_loss(self, batch, forwards=True):
         
         if self. burn_in:
             self.batch_terminal_empirical_burn = next(self.data_stream(forward=forwards))
@@ -203,81 +147,80 @@ class cIPFP(object):
         H = self.H_1 if forwards else self.H_0
 
         return self.inner_loss_jit(
-            theta, batch,
-            batch_terminal_empirical, H ,  forwards
+            batch, batch_terminal_empirical,
+            forwards
         )
 
-#     @partial(jit, static_argnums=(0,1, 4))
-    def update(self, i, opt_state, batch, forwards=True):
-
-        get_params = self.get_params_f if forwards else self.get_params_b
-        params = get_params(opt_state)
+    def closure(self, X, forwards=True):
+        """
+        Close method required for pytorches CG and LBFGS optimizers
+        :params X[torch.tensor]: nxdim  torch tensor representing dataset
+        """
         
-        gradient = grad(self.inner_loss)(params, batch, forwards)
+        opt = self.optimizer_f if forwards else self.optimizer_b
+        
+        opt.zero_grad()
+        loss = self.inner_loss(X, forwards)
+        loss.backward()
+        return loss
     
-        opt_update  = self.opt_update_f if forwards else self.opt_update_b
-        return opt_update(i, gradient, opt_state)
-
+    
     def fit(self, IPFP_iterations=10, sub_iterations=10, plot=False, burn_iterations=7):     
         
-        _, init_params_f = self.b_forward_init(self.rng, (-1, self.dim + 1))                                             
-        opt_state_f = self.opt_init_f(init_params_f)
+        name = str(self.optimizer_f).lower()
+        bfgs_cg_flag  = "bfgs" in name or "cg" in name
+        close_f, close_b =  [], []
+
         
-        _, init_params_b = self.b_backward_init(self.rng_b, (-1, self.dim + 1))                                               
-        opt_state_b = self.opt_init_b(init_params_b)
-        
-        batches_f = self.data_stream(forward=True)
-        batches_b = self.data_stream(forward=False)
-        
-        loss_f, loss_b = [], []
-        
-        self.theta_f = self.get_params_f(opt_state_f)
-        self.theta_b = self.get_params_b(opt_state_b)
         
         if plot: self.plot_trajectories()
         
         self.first_iter_fac = 0.0
         if burn_iterations > 0 : self.burn_in = True
             
-            
+        
+        batches_f = self.data_stream(forward=True)
+        batches_b = self.data_stream(forward=False)
+        
+        loss_b, loss_f = [], []
         for i in tqdm(range(IPFP_iterations + burn_iterations)):
             
             if i == burn_iterations:
                 self.burn_in = False
                 self.first_iter_fac = 0.0
-
-            itercount = itertools.count()
-
+            
+            
             for k in range(sub_iterations):
                 for _ in range(self.num_batches_b):
                     batch_b =  next(batches_b)
-                    opt_state_b  = self.update(
-                        next(itercount), opt_state_b, batch_b, False
+                    
+                    if bfgs_cg_flag:
+                        close_b = [lambda : self.closure(batch_b, False)]
+                    lossb = self.inner_loss(
+                        batch_b, False
                     )
-
-                params_b = self.get_params_b(opt_state_b)
-                lossb = self.inner_loss(params_b, batch_b, False)
+                    lossb.backward()
+                    self.optimizer_b.step(*close_b)
                 print(f"loss b {lossb}")
             loss_b.append(lossb)
 
-            self.theta_b = params_b
             self.first_iter_fac = 1.0
-
-            itercount = itertools.count()
-
+       
             for k in range(sub_iterations):
                 for _ in range(self.num_batches_f):
                     batch_f =  next(batches_f)
-                    opt_state_f = self.update(
-                        next(itercount), opt_state_f, batch_f, True
+                    if bfgs_cg_flag:
+                        close_f = [lambda : self.closure(batch_f, True)]
+                    lossf = self.inner_loss(
+                        batch_f, True
                     )
-                params_f = self.get_params_f(opt_state_f)
-                lossf = self.inner_loss(params_f, batch_f, True)
+                    
+                    lossf.backward()
+                    self.optimizer_f.step(*close_f)
+  
                 print(f"loss f {lossf}")
             loss_f.append(lossf)
 
-
-            self.theta_f = params_f
 
             self.loss_f = loss_f
             self.loss_b = loss_b
@@ -290,16 +233,17 @@ class cIPFP(object):
         plt.show()
         
     def plot_trajectories(self):
-        bb = lambda X, theta: -self.b_backward(X, theta)
+        bb = lambda X: -self.b_backward(X)
+
         t, Xts = c.sde_solver(
-            self.b_forward, self.sigma,
-            self.X_0, self.dt,  self.number_time_steps, 0,
-            self.rng, self.theta_f, False, True
+            self.b_forward.forward, self.sigma,
+            self.X_0, self.dt,  self.number_time_steps, 0.0,
+            self.theta_f, False, True
         )
         t_, Xts_ = c.sde_solver(
             bb, self.sigma, 
             self.X_1, self.dt, self.number_time_steps, 0.0, 
-            self.rng, self.theta_b, False, False
+            self.theta_b, False, False
         )
         
         fn = 14
@@ -310,16 +254,16 @@ class cIPFP(object):
 
         tt = axs[1].get_xticks()
         axs[1].set_xticks(tt.flatten() )
-        axs[1].set_xticklabels(list(map (lambda x: '{0:.1f}'.format((x)), tt))[::-1])
+        axs[1].set_xticklabels(list(map (lambda x: '{0:.2f}'.format((x)), tt))[::-1])
 
         for i in range(n):
             label = "$\mathbb{Q}$: Forward process" if i == 0 else None
-            axs[0].plot(t.flatten(), Xts[i,:,:-1].flatten(), 'b', alpha=0.03,  label=label)
+            axs[0].plot(t.cpu().flatten(), Xts[i,:,:-1].detach().cpu().numpy().flatten(), 'b', alpha=0.03,  label=label)
 
 
         for i in range(n):
             label = "$\mathbb{P}$: Reverse process" if i == 0 else None
-            axs[1].plot(t_.flatten(), Xts_[i,:, :-1].flatten(), 'r', alpha=0.03, label=label)
+            axs[1].plot(t_.cpu().flatten(), Xts_[i,:, :-1].detach().cpu().numpy().flatten(), 'r', alpha=0.03, label=label)
 
         axs[1].legend(fontsize=fn)
         axs[0].legend(fontsize=fn)
@@ -337,3 +281,4 @@ class cIPFP(object):
         )
 
         return Xt[:,-1,:]
+            
